27ed799676e0876f613c7798af14285b1b237d00
Toma <13.35.104@gmail.com>
2021-11-23 15:07:26 +0900

    画像を表示できるようにした

--- renderer.js
+++ renderer.js
@@ -2,9 +2,14 @@ window.addEventListener('DOMContentLoaded', async () => {
     const rootDir = "/tmp/sample";
     const fileNames = await window.api.getImagesAndDirs(rootDir);
     const fileListElem = document.getElementById('file_list')
-    fileNames.forEach(fileName => {
+    fileNames.forEach((fileName) => {
         const element = document.createElement('li')
         element.innerText = fileName
+        element.addEventListener('click', (e) => {
+            document.getElementById('selected_file').innerText = e.target.innerText
+            const imagePath = document.getElementById('dir_name').innerText + '/' + e.target.innerText
+            document.getElementById('image').src = imagePath
+        })
         fileListElem.appendChild(element)
     });
     document.getElementById('dir_name').innerText = rootDir
