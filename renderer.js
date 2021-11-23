f049f728799feac190469b079704bc1eb3cc5214
Toma <13.35.104@gmail.com>
2021-11-23 16:13:19 +0900

    ディレクトリ表示

--- renderer.js
+++ renderer.js
@@ -8,14 +8,35 @@ window.addEventListener('DOMContentLoaded', async () => {
         element.dataset.fullPath = file.full_path
         element.dataset.isDir = file.is_dir
         element.addEventListener('click', (e) => {
-            document.getElementById('selected_file').innerText = e.target.innerText
-            if (e.target.dataset.isDir == "true") {
-                document.getElementById('image').src = ""
-            } else {
-                document.getElementById('image').src = e.target.dataset.fullPath
-            }
+            addEventListener(e)
         })
         fileListElem.appendChild(element)
     });
     document.getElementById('dir_name').innerText = rootDir
 })
+
+
+// ファイル・ディレクトリクリック時のイベントを追加
+const addEventListener = async (e) => {
+    document.getElementById('selected_file').innerText = e.target.innerText
+
+    const imageElem = document.getElementById('image')
+    const imageListElem = document.getElementById('image_list')
+
+    if (e.target.dataset.isDir == "true") {
+        imageElem.style.display = "none"
+        imageListElem.style.display = ""
+        imageListElem.innerHTML = ""
+        const fileList = await window.api.getImagesAndDirs(e.target.dataset.fullPath);
+        fileList.forEach((file) => {
+            const element = document.createElement('img')
+            element.src = file.full_path
+            element.width = 200
+            imageListElem.appendChild(element)
+        });    
+    } else {
+        imageElem.style.display = ""
+        imageListElem.style.display = "none"
+        imageElem.src = e.target.dataset.fullPath
+    }
+}
