0b70c0dcefdbc136bdfb0fc39eb64062c50667fc
Toma <13.35.104@gmail.com>
2021-11-23 17:01:28 +0900

    不要な処理を削除

--- run_py.js
+++ run_py.js
@@ -1,11 +1,7 @@
 const { PythonShell } = require('python-shell');
 
-let options = {
-  pythonOptions: ['-u'], // get print results in real-time
-};
 
 PythonShell.run('./py/hello.py', options, function (err, results) {
   if (err) throw err;
-  // results is an array consisting of messages collected during execution
   console.log('results: %j', results);
 });
