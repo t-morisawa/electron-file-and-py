6dd9cd8b59e0dacc1a5dd027280e814036faa1bd
Toma <13.35.104@gmail.com>
2021-11-23 15:47:53 +0900

    リファクタリング

--- file.js
+++ file.js
@@ -1,7 +1,21 @@
 const fs = require('fs');
+const path = require('path');
 
-const get_images_and_dirs = (path) => {
-  return fs.readdirSync(path);
+const get_images_and_dirs = (dir) => {
+  fileNames = fs.readdirSync(dir);
+  let results = []
+
+  fileNames.forEach(fileName => {
+    const fullPath = path.join(dir, fileName)
+    const stats = fs.statSync(fullPath)
+    const result = {
+      "name": fileName,
+      "full_path": fullPath,
+      "is_dir": stats.isDirectory()
+    }
+    results.push(result)
+  });
+  return results
 }
 
 module.exports = {
--- renderer.js
+++ renderer.js
@@ -1,14 +1,14 @@
 window.addEventListener('DOMContentLoaded', async () => {
     const rootDir = "/tmp/sample";
-    const fileNames = await window.api.getImagesAndDirs(rootDir);
+    const fileList = await window.api.getImagesAndDirs(rootDir);
     const fileListElem = document.getElementById('file_list')
-    fileNames.forEach((fileName) => {
+    fileList.forEach((file) => {
         const element = document.createElement('li')
-        element.innerText = fileName
+        element.innerText = file.name
+        element.dataset.fullPath = file.full_path
         element.addEventListener('click', (e) => {
             document.getElementById('selected_file').innerText = e.target.innerText
-            const imagePath = document.getElementById('dir_name').innerText + '/' + e.target.innerText
-            document.getElementById('image').src = imagePath
+            document.getElementById('image').src = e.target.dataset.fullPath
         })
         fileListElem.appendChild(element)
     });
