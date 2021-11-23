188f057758769bbb06dc83c105ddc047053827fb
Toma <13.35.104@gmail.com>
2021-11-23 17:42:16 +0900

    export

--- run_py.js
+++ run_py.js
@@ -1,7 +1,13 @@
 const { PythonShell } = require('python-shell');
 
 
-PythonShell.run('./py/hello.py', options, function (err, results) {
-  if (err) throw err;
-  console.log('results: %j', results);
-});
+const run_py = () => {
+  PythonShell.run('./py/hello.py', options, function (err, results) {
+    if (err) throw err;
+    return results;
+  });
+}
+
+module.exports = {
+  run_py
+}
